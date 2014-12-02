2e2e14cadaf14984866496ff8ad5a4ac26d95446
augre <augre.augre@gmail.com>
2014-12-01 22:19:46 -0600

    bugfix

--- webgui.py
+++ webgui.py
@@ -81,7 +81,7 @@ def create_table(rows):
 def print_graph_script(table, title0, title1):
 
     # google chart snippet
-    chart_code="""
+    chart_code=("""
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
       google.load("visualization", "1", {packages:["corechart"]});
@@ -99,7 +99,7 @@ def print_graph_script(table, title0, title1):
         var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-    </script>"""
+    </script>""")
 
     print chart_code % (table)
 
